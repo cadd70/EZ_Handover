1c903ac25e0c6a6b5c8b1173c6a2f9f7101aa58b
cadd70 <cadd70@gmail.com>
2020-03-23 00:15:45 -0300

    criacao da tabela e form de cadastro

--- app/renderer.js
+++ app/renderer.js
@@ -1,5 +1,33 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('database.sqlite');
+var db = new sqlite3.Database('./database.sqlite');
+
+/*************\
+| Cria tabela |
+\*************/
+
+// db.run("DROP TABLE tbl_perfil_cliente;");
+
+// db.run(
+//     "CREATE TABLE tbl_perfil_cliente (" +
+//         "id INTEGER NOT NULL PRIMARY KEY," +
+//         "nome_cliente TEXT NOT NULL," +
+//         "contatos_cliente TEXT NOT NULL," +
+//         "contatos_origem TEXT NOT NULL," +
+//         "tipo_embarque TEXT NOT NULL," +
+//         "tipo_frete_negociado TEXT NOT NULL," +
+//         "cliente_aprova_hbl TEXT NOT NULL," +
+//         "envio_ri TEXT NOT NULL," +
+//         "tem_seguro TEXT NOT NULL," +
+//         "taxas_destino TEXT NOT NULL," +
+//         "desembaraco_ceva TEXT NOT NULL," +
+//         "embarques_mes INTEGER NOT NULL," +
+//         "freetime TEXT NOT NULL," +
+//         "observacoes TEXT," +
+//         "criada_por TEXT NOT NULL" +
+//     ");"
+// );
+
+// db.close();
 
 // db.serialize(function() {
 //   db.run("CREATE TABLE lorem (info TEXT)");
@@ -21,8 +49,11 @@ var db = new sqlite3.Database('database.sqlite');
 
 // db.close();
 
-// RESETAR CAMPOS DO FORM DE CADASTRO DE DEMANDA DE DISTRATO AO FECHAR O MODAL
+/******************************************\
+| RESETAR CAMPOS DO FORM AO FECHAR O MODAL |
+\******************************************/
 
 $(".modal").on('hidden.bs.modal', function(e){
     $(this).find("form")[0].reset();       
 });
+
